9fee555f9fc2c9f6997c1bb819870858d6b9184d
MattSathler <mattsathler@gmail.com>
2025-07-20 15:42:59 -0300

    fix(core): add single profile update command

--- src/bots/momento-core/Commands/TextCommands/reloadusers/reloadusers.ts
+++ src/bots/momento-core/Commands/TextCommands/reloadusers/reloadusers.ts
@@ -18,13 +18,20 @@ export const reloadUsers: ICommand
 
     exec: async function (ctx: IContext, message: Message): Promise<void> {
         const split = message.content.trim().split(/\s+/); // separa por espaço
-        const userId = split[1];
-        const profiles = await ctx.mongoService.get("users", { 'userId': userId }) as User[];
         const profileService: ProfileServices = new ProfileServices();
-        profiles.forEach(async profile => {
-            console.log("Updating user", profile.username);
+        const userId = split[1];
+
+        if (userId) {
+            const profiles = await ctx.mongoService.get("users", { 'userId': userId }) as User[];
+            profiles.forEach(async profile => {
+                console.log("Updating user", profile.username);
+                await profileService.updateProfilePictures(ctx, profile, true, true);
+            })
+        }
+        else {
+            const profile = await ctx.mongoService.getOne("users", { 'references.channelId': message.channelId }) as User;
             await profileService.updateProfilePictures(ctx, profile, true, true);
-        })
+        }
 
         return;
     }
