1920f798f0b988a13633833d37abf1553831c822
root <root@MatheusPC.localdomain>
2025-09-25 10:25:38 -0300

    feat: update payment options and subscription types in updateInfo

--- updateInfo.ts
+++ updateInfo.ts
@@ -1,8 +1,5 @@
 export const updateInfo = {
-    description: "A imagem de background do perfil chegou na tematização do momento! Agora, além de customizar as cores do seu tema, você pode adicionar uma imagem de background personalizada para o seu perfil. Assim, você pode deixar o seu perfil ainda mais com a sua cara e destacar-se na comunidade. Aproveite essa nova funcionalidade para expressar sua personalidade e tornar seu perfil único!",
-    features: ["🎨 Adição de imagem de background personalizada para o perfil;"],
-    fixes: [
-        "🔧 Corrige as mensagens de erro ao criar temas;",
-        "🔧 Melhora a validação de URLs para imagens de background;",
-    ]
+    description: "Novas formas de pagamentos e tipos de assinaturas!",
+    features: ["🎨 Adição de um modelo dinâmico de assinatura, dando a possibilidade de descontos periódicos!;"],
+    fixes: []
 }
